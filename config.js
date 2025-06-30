53c3ba983d68b8897ed3b51c5c0a16e0eef779ff
razzlerazing3 <deepthereepskelly@gmail.com>
2025-06-30 21:59:56 +0000

    dasdaw

--- config.js
+++ config.js
@@ -6,10 +6,6 @@ const config = {
 };
 
 export default config;
-let _CONFIG = {
-  wispurl: localStorage.getItem("proxServer") || "wss://anura.pro/",
-  bareurl: "https://aluu.xyz/bare/",
-};
 // What are you doing here if you are here u are 🏳️‍🌈
 // jk u are not THAT but still use npm start and then npm start
 // share this w/ ur friends :D
--- sixty_nine/config.js
+++
@@ -1,4 +0,0 @@
-let _CONFIG = {
-  wispurl: localStorage.getItem("proxServer") || "wss://anura.pro/",
-  bareurl: "https://aluu.xyz/bare/",
-};
